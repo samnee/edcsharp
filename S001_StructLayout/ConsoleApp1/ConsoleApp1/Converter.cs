d904220e133d60f53db1d5083d7c74e2be7d3e46
nzg <nizugao001@163.com>
2017-11-06 21:05:05 +0800

    S001-Just for synchronization , not build !!!

---
+++ S001_StructLayout/ConsoleApp1/ConsoleApp1/Converter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;   // Marshal
+
+namespace ConsoleApp1
+{
+    class Converter
+    {
+        public Byte[] StructToBytes(Object structure)
+        {
+            Int32 size = Marshal.SizeOf(structure);
+            Console.WriteLine(size);
+            IntPtr buffer = Marshal.AllocHGlobal(size);
+            try
+            {
+                Marshal.StructureToPtr(structure, buffer, false);
+                Byte[] bytes = new Byte[size];
+                Marshal.Copy(buffer, bytes, 0, size);
+                return bytes;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(buffer);
+            }
+        }
+        public Object BytesToStruct(Byte[] bytes, Type strcutType)
+        {
+            Int32 size = Marshal.SizeOf(strcutType);
+            IntPtr buffer = Marshal.AllocHGlobal(size);
+            try
+            {
+                Marshal.Copy(bytes, 0, buffer, size);
+                return Marshal.PtrToStructure(buffer, strcutType);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(buffer);
+            }
+        }
+    }
+}
---
+++ S001_StructLayout/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;  // StructLayout 
+
+// REF: http://www.cnblogs.com/cnxkey/articles/3852591.html
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter Convert = new Converter();
+
+            my_message m;
+
+            m = new my_message("yanlina");
+            m.cmd_type = 1633837924;
+            m.srcID = 1633837924;
+            m.dstID = 1633837924;
+
+            Byte[] message = Convert.StructToBytes(m);
+
+            my_message n = (my_message)Convert.BytesToStruct(message, m.GetType());
+
+            Console.WriteLine(Encoding.ASCII.GetString(message));
+            Console.WriteLine(n.username);
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct my_message
+        {
+            public UInt32 cmd_type;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+            public string username;
+            public UInt32 dstID;
+            public UInt32 srcID;
+            public my_message(string s)
+            {
+                cmd_type = 0;
+                username = s;
+                dstID = 0;
+                srcID = 0;
+            }
+        }
+    }
+}
