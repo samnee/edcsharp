5160c7f928d85be5c98372417b477f0a5633e8b7
nizugao <nizugao001@163.com>
2017-11-18 00:43:16 +0800

    S010-实现 双向绑定和单向绑定。

--- S010/databinding/Form1.Designer.cs
+++ S010/databinding/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1-双向绑定";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2-单向绑定";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- S010/databinding/Form1.cs
+++ S010/databinding/Form1.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Linq.Expressions;
+
+// REF: https://www.cnblogs.com/Xavierr/archive/2011/12/12/2284891.html
+
 namespace databinding
 {
     public partial class Form1 : Form
@@ -16,5 +20,50 @@ namespace databinding
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _myData = new MyData();
+            textBox1.DataBindings.Add("Text", _myData, "TheValue", false, DataSourceUpdateMode.OnPropertyChanged);
+            textBox2.DataBindings.Add("Text", _myData, "TheValue", false, DataSourceUpdateMode.Never);
+        }
+
+        MyData _myData;
     }
+
+    public class MyData : INotifyPropertyChanged
+    {
+        private string _theValue = string.Empty;
+
+        public string TheValue
+        {
+            get { return _theValue; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) && value == _theValue)
+                    return;
+
+                _theValue = value;
+                NotifyPropertyChanged(() => TheValue);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged<T>(Expression<Func<T>> property)
+        {
+            if (PropertyChanged == null)
+                return;
+
+            var memberExpression = property.Body as MemberExpression;
+            if (memberExpression == null)
+                return;
+
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(memberExpression.Member.Name));
+        }
+
+       
+
+    }
+
 }
